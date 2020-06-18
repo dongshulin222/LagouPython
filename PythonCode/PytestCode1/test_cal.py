302eff8d7800fdf4ba8b281ec322b0f5d0fd8054
dongshulin222 <dongshulin0222>
2020-06-18 21:33:42 +0800

    Pytest实战一  修改用例和方法名

--- PythonCode/PytestCode1/test_cal.py
+++ PythonCode/PytestCode1/test_cal.py
@@ -8,13 +8,13 @@ class TestCal:
         assert r == calculator_start.add(a, b)
 
     @pytest.mark.parametrize('a,b,r', yaml.safe_load(open('CalData_Sub.yml', encoding='utf-8')))
-    def test_add(self, calculator_start, a, b, r):
+    def test_sub(self, calculator_start, a, b, r):
         assert r == calculator_start.sub(a, b)
 
     @pytest.mark.parametrize('a,b,r', yaml.safe_load(open('CalData_Multiply.yml', encoding='utf-8')))
-    def test_add(self, calculator_start, a, b, r):
+    def test_multiply(self, calculator_start, a, b, r):
         assert r == calculator_start.multiply(a, b)
 
     @pytest.mark.parametrize('a,b,r', yaml.safe_load(open('CalData_Div.yml', encoding='utf-8')))
-    def test_add(self, calculator_start, a, b, r):
+    def test_div(self, calculator_start, a, b, r):
         assert r == calculator_start.div(a, b)
