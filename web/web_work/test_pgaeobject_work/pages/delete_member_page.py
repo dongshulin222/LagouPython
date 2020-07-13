d618232fb52590a7b7317bd0df2c8ad7e2272315
dongshulin222 <dongshulin0222>
2020-07-13 22:56:26 +0800

    企业微信实战二 作业上传

--- web/web_work/test_pgaeobject_work/pages/delete_member_page.py
+++ web/web_work/test_pgaeobject_work/pages/delete_member_page.py
@@ -14,18 +14,16 @@ class DeleteMemberPage(BasePage):
     def delete_member(self, _phone):
         contact = ContactPage()
         phonelist = contact.get_member()
-        i = 1
+        # 可以使用另一种方式，获取姓名，再获取姓名的父节点，再通过父节点去过去删除按钮
         if self._phone in phonelist:
-            for ele in phonelist:
-                if self._phone == ele:
-                    self.find(By.XPATH, "//*[@class='js_has_member']/table/tbody[2]/tr/td[1]").click()
-                    self.find(By.CSS_SELECTOR,
-                              "div.js_has_member > div:nth-child(1) > a.qui_btn.ww_btn.js_delete").click()
-                i = i + 1
+            self.find(By.XPATH, f"//*[@title='{self._phone}']/../td[1]").click()
             # 确定删除按钮出现
-            WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(
-                (By.XPATH, "//*[@id='__dialog__MNDialog__']/div/div[3]/a[1]")))
-            self.find(By.XPATH, "//*[@id='__dialog__MNDialog__']/div/div[3]/a[1]").click()
+            delete_locator = (By.XPATH, "//*[@class='js_has_member']/div[9]/a[3]")
+            WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(delete_locator))
+            self.find(*delete_locator).click()
+            confirm_locator = (By.XPATH, "//*[@id='__dialog__MNDialog__']/div/div[3]/a")
+            WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(confirm_locator))
+            self.find(*confirm_locator).click()
             self.refresh()
         else:
             print("要删除的手机号在通讯录中不存在")
